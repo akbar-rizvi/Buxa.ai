c7ea217d471daf0ed9667085e8f5713d0dd792dd
Ayushchaturvedi1 <ayushdrew4@gmail.com>
2024-11-04 19:39:18 +0530

    updated article function

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -10,13 +10,13 @@ export const users:any = pgTable('userss', {
     phoneNumber: varchar('phone_number',{length:10}).notNull(),
     password: varchar('password', { length: 255 }).notNull(),
     credits:integer("credits").default(50),
-    usedCredits:integer("used_credits").default(0),
-    totalContent:integer("total_content").default(0),
-    totalResearch:integer("total_research").default(0),
-    totalAlerts:integer("total_alerts").default(0),
-    coc:integer("credits_on_content").default(0),
-    cor:integer("credits_on_research").default(0),
-    coa:integer("credits_on_alerts").default(0),
+    // usedCredits:integer("used_credits").default(0),
+    // totalContent:integer("total_content").default(0),
+    // totalResearch:integer("total_research").default(0),
+    // totalAlerts:integer("total_alerts").default(0),
+    // coc:integer("credits_on_content").default(0),
+    // cor:integer("credits_on_research").default(0),
+    // coa:integer("credits_on_alerts").default(0),
     refreshToken: text('refresh_token'),
     createdAt: timestamp('created_at').default(sql`NOW()`),
     updatedAt: timestamp('updated_at').default(sql`NOW()`),
@@ -40,21 +40,18 @@ export const documents:any = pgTable('documents', {
   pk: primaryKey({ columns: [table.id] }),
 }));
 
-export const alert=pgTable("alert",{
-  id:serial("id"),
-  userId: integer('user_id').references(() => users.id),
-  alertId:varchar("alert_id"),
-  metaData:jsonb('metadata'),
-  alertContent:varchar('alert_content'),
-  // alertStatus:varchar("alert_status"),//ongoing,expired
-  // fromDate:timestamp('from_date'),
-  // toDate:timestamp("to_date"),
-  isDeleted: boolean('is_deleted').default(false),
-  isFavorite: boolean('is_favorite').default(false),
-  createdAt: timestamp('created_at').default(sql`NOW()`),
-}, (table) => ({
-  pk: primaryKey({ columns: [table.id] }),
-}))
+// export const alert=pgTable("alert",{
+//   id:serial("id"),
+//   userId: integer('user_id').references(() => users.id),
+//   alertId:varchar("alert_id"),
+//   metaData:jsonb('metadata'),
+//   alertContent:varchar('alert_content'),
+//   isDeleted: boolean('is_deleted').default(false),
+//   isFavorite: boolean('is_favorite').default(false),
+//   createdAt: timestamp('created_at').default(sql`NOW()`),
+// }, (table) => ({
+//   pk: primaryKey({ columns: [table.id] }),
+// }))
 
 export const payment:any=pgTable("payment",{
   id:serial("id"),
@@ -92,11 +89,11 @@ export const documentsRelations = relations(documents, ({ one }) => ({
   })
 }));
 
-export const alertRelations = relations(alert, ({ one }) => ({
-  user: one(users, {
-    fields: [alert.userId],
-    references: [users.id],
-    relationName: 'alerts'
-  })
-}))
+// export const alertRelations = relations(alert, ({ one }) => ({
+//   user: one(users, {
+//     fields: [alert.userId],
+//     references: [users.id],
+//     relationName: 'alerts'
+//   })
+// }))
 
